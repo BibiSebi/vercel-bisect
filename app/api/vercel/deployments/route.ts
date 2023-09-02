5d69203b094e51176b7c1145f47f2a14e5c89223
Bibiana Sebestianova <b.sebestianova@gmail.com>
2023-09-02 16:52:18 +0200

    feat: code changes, todos as comments

--- app/api/vercel/deployments/route.ts
+++ app/api/vercel/deployments/route.ts
@@ -8,36 +8,63 @@ const projectId = process.env.PROJECT_ID;
 // since: Get Deployments created after this JavaScript timestamp. (bad)
 // until: Get Deployments created before this JavaScript timestamp. (good)
 export async function GET(request: Request, response: Response) {
+  //GET deployments between good and bad
   const deployments = await getPaginatedDeployments(
     String(badDeployment),
     String(goodDeployment + 1),
   );
+  //GET all deployments of a project
+  const allDeployments = await getPaginatedDeployments();
   return NextResponse.json(deployments);
 }
 
-// @ts-ignore
-const getPaginatedDeployments = async (since: string, until: string) => {
+type GetPaginatedDeployments = (since?: string, until?: string) => Promise<any>;
+const getPaginatedDeployments: GetPaginatedDeployments = async (
+  since,
+  until,
+) => {
   const deploymentsRes = await fetchDeployments(since, until);
 
   if (!deploymentsRes.deployments) {
     return [];
   }
 
+  const deployments: any[] = deploymentsRes.deployments;
+
   if (deploymentsRes.pagination.next === null) {
-    return deploymentsRes.deployments;
+    return deployments;
   }
 
   // @ts-ignore
+  //TODO: set limit to 100
   const nextDeployments = await getPaginatedDeployments(
     since,
     deploymentsRes.pagination.next,
   );
 
-  return [...deploymentsRes.deployments, ...nextDeployments];
+  if (
+    nextDeployments &&
+    Array.isArray(nextDeployments) &&
+    nextDeployments.length > 0
+  ) {
+    deployments.push(nextDeployments);
+  }
+
+  return deployments;
 };
 
-const fetchDeployments = (since: string, until: string) => {
+type FetchDeployments = (since?: string, until?: string) => Promise<any>;
+const fetchDeployments: FetchDeployments = (since, until) => {
   const token = cookies().get("vercel");
+  const query = {
+    target: "production",
+    until,
+    since,
+    projectId,
+  };
+  //TODO: build query depending on the paramaters given
+
+  //TODO: error handling
   return fetch(
     `https://api.vercel.com/v6/deployments?target=production&since=${since}&until=${until}&projectId=${projectId}`,
     {
@@ -45,5 +72,7 @@ const fetchDeployments = (since: string, until: string) => {
         Authorization: `Bearer ${token?.value}`,
       },
     },
-  ).then((res) => res.json());
+  )
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
 };
