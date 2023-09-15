6efd89174caa8a8e3552ad7641dea1e154d81104
Laurenz Honauer <laurenz.honauer@gmail.com>
2023-09-15 15:18:11 +0200

    feat: conditionally show login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,49 @@
-"use client";
-
+import { cookies } from "next/headers";
 import Link from "next/link";
 
 export default function Home() {
+  const vercelCookie = cookies().get("vercel");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://vercel.com/integrations/vercel-bisect/new"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Vercel Log in
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Connect your Vercel Account
-          </p>
-        </a>
-      </div>
+      {!vercelCookie && (
+        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <a
+            href="https://vercel.com/integrations/vercel-bisect/new"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Vercel Log in
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Connect your Vercel Account
+            </p>
+          </a>
+        </div>
+      )}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link
-          href="/projects"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Start Bisecting{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Select a Project and find the first broken deployment via vercel
-            bisect
-          </p>
-        </Link>
-      </div>
+      {vercelCookie && (
+        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+          <Link
+            href="/projects"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Start Bisecting{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Select a Project and find the first broken deployment via vercel
+              bisect
+            </p>
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
--- app/projects/[project]/bisect/page.tsx
+++ app/projects/[project]/bisect/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Deployment } from "@/lib/vercel";
+import Link from "next/link";
 import { redirect, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -16,7 +18,7 @@ export default function Bisect({ params }: { params: { project: string } }) {
   const [currentDeployment, setCurrentDeployment] = useState<any | undefined>(
     undefined,
   );
-  const [result, setResult] = useState<any | undefined>(undefined);
+  const [result, setResult] = useState<Deployment | undefined>(undefined);
 
   useEffect(() => {
     fetch(
@@ -70,18 +72,21 @@ export default function Bisect({ params }: { params: { project: string } }) {
   };
 
   return (
-    <main className="flex w-screen h-screen">
+    <main className="flex w-screen flex-col h-screen">
+      <Link href="/">{"<-"} Go Back Home</Link>
       {currentDeployment && (
         <div className={"flex flex-1 flex-col"}>
           {currentDeployment && (
-            <span>Current {currentDeployment.meta.githubCommitMessage}</span>
+            <h1 className=" text-3xl p-4 text-center">
+              Current {currentDeployment.meta.githubCommitMessage}
+            </h1>
           )}
           <iframe
             className="w-full h-full flex flex-1"
             referrerPolicy="no-referrer"
             src={`https://${currentDeployment.url}/`}
           />
-          <div>
+          <div className="flex justify-center">
             <button
               onClick={() => markDeployment("good")}
               className="border border-1 border-pink-500 p-2"
@@ -97,17 +102,32 @@ export default function Bisect({ params }: { params: { project: string } }) {
           </div>
 
           {result && (
-            <div>
-              Result
-              <a target="_blank" href={`https://${result.url}`}>
-                {result.url}
-              </a>
-              {result.meta.githubCommitSha}
-              <a
-                href={`https://github.com/${result.meta.githubCommitOrg}/${result.meta.githubCommitRepo}/commit/${result.meta.githubCommitSha}`}
-              >
-                {result.meta.githubCommitSha}
-              </a>
+            <div className="flex-col items-center justify-between p-24">
+              This is the first broken deployment:
+              <div className="mb-32 flex border-2 border-white p-4 rounded-md justify-around lg:text-left flex-wrap">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="className={`m-0 max-w-[30ch] text-sm opacity-50`}"
+                  href={`https://${result.url}`}
+                >
+                  <span className={`mb-3 text-2xl font-semibold`}>
+                    {result.url}
+                  </span>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="className={`m-0 max-w-[30ch] text-sm opacity-50`}"
+                  href={`https://github.com/${result.meta.githubCommitOrg}/${result.meta.githubCommitRepo}/commit/${result.meta.githubCommitSha}`}
+                >
+                  {result.meta.githubCommitSha}
+                </a>
+                <span>
+                  {new Date(result.createdAt).toDateString()}
+                  {new Date(result.createdAt).toTimeString()}
+                </span>
+              </div>
             </div>
           )}
         </div>
